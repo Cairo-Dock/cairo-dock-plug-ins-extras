198e0f2cb5c1a900548451020007c9ba0524183c
matttbe <matttbe@gmail.com>
2010-04-03 12:28:17 +0200

    dependences_deb: log into the right file

--- dependences_deb.sh
+++ dependences_deb.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-NEEDED="ruby1.8-dev ruby1.8 ri1.8 rdoc1.8 irb1.8 libreadline-ruby1.8 libruby1.8 rubygems1.8 acpi python"
+NEEDED_EXTRAS="ruby1.8-dev ruby1.8 ri1.8 rdoc1.8 irb1.8 libreadline-ruby1.8 libruby1.8 rubygems1.8 acpi python"
 
 NORMAL="\\033[0;39m"
 BLEU="\\033[1;34m"
 VERT="\\033[1;32m" 
 ROUGE="\\033[1;31m"
 
-install_pkg () {
-	for tested in $NEEDED; do
+LOG_CD=../log.txt
+
+install_pkg() {
+	for tested in $NEEDED_EXTRAS; do
 		dpkg -s $tested |grep installed |grep "install ok" > /dev/null
 		if [ $? -eq 1 ]; then
-			sudo apt-get install -qq $tested  >> $LOG_CAIRO_DOCK
+			sudo apt-get install -qq $tested  >> $LOG_CD
 		fi
 	done
 }
 
-install_pkg > /dev/null
+install_pkg >> $LOG_CD
 
 	# RubyBattery
 if ! test -e '../.gem'; then
