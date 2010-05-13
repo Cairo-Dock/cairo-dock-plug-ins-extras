53adf68d3df3ec396729dace06ba75af65b5125c
eduardo-mucelli <edumucelli@gmail.com>
2010-05-12 22:42:09 -0300

    A heavy code refactoring, each search engine has its own module. Webshots has changed its HTML the whole fetching was recoded. Removing user-defined option for a pre-fetching limit. Finally a totally functional parallelized thumbnail download.

---
+++ WebSearch/lib/Webshots.rb
@@ -0,0 +1,27 @@
+class Webshots < Engine
+	
+	def initialize
+		self.base_url = "http://www.webshots.com"
+		self.query_url = "#{self.base_url}/search?querySource=community&query="								# 36 results per page
+		super
+	end
+
+	# url, e.g, http://good-times.webshots.com/photo/2500137270102572130
+	# thumb_url, e.g, http://thumb10.webshots.net/t/24/665/1/37/27/2500137270102572130SmNoHt_th.jpg"
+	def retrieve_links(query, offset = 0)
+		webshots = Nokogiri::HTML(open("#{self.query_url}#{query}&start=#{offset}"))
+		self.stats = retrieve_webshots_result_stats(webshots, query)
+		(webshots/"a[@class='searchListItemLink']").each do |res|
+			url = res['href']
+			description = res['title']
+			thumb_url = res.at("img[@class='searchListItemImg']")['src']
+			self.links << ThumbnailedLink.new(url, description, thumb_url)
+		end
+		self.links
+	end
+
+	def retrieve_webshots_result_stats(webshots, query)
+		total = webshots.at("span[@class='resultsNo']/strong").inner_text
+		"Search for #{query} returned #{total} results"
+	end
+end
---
+++ WebSearch/lib/Youtube.rb
@@ -0,0 +1,28 @@
+class Youtube < Engine
+	
+	def initialize
+		self.base_url = "http://www.youtube.com"
+		self.query_url = "#{self.base_url}/results?search_query="						# 20 results per page
+		super
+	end
+
+	# url, e.g, /watch?v=WwojCsQ3Fa8
+	# thumb_url, e.g, "http://i4.ytimg.com/vi/WwojCsQ3Fa8/default.jpg"
+	def retrieve_links(query, page = 1)
+		youtube = Nokogiri::HTML(open("#{self.query_url}#{query}&page=#{page}"))
+		self.stats = retrieve_youtube_result_stats(youtube, query)
+		(youtube/"a[@id^='video-long-title-']").each do |res|							# 'a' tag has id which starts with "video-long-title-"
+			url = res['href']
+			description = res.inner_text
+			video_id = url.split('=').last												# /watch?v=WwojCsQ3Fa8 => WwojCsQ3Fa8 => video_id
+			thumb_url = "http://i4.ytimg.com/vi/#{video_id}/default.jpg"
+			self.links << ThumbnailedLink.new("#{self.base_url}#{url}", description, thumb_url)
+		end
+		self.links
+	end
+
+	def retrieve_youtube_result_stats (youtube, query)
+		total = youtube.at("div[@class='name']").inner_text.split.last
+		"Search for #{query} returned #{total} results"
+	end
+end
