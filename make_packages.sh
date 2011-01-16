b5cf4a2ddc4f88e945ffe487d650ffee1fb2b9d0
matttbe <matttbe@gmail.com>
2011-01-16 15:05:04 +0100

    make_packages.sh: create the FTP directory

--- make_packages.sh
+++ make_packages.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 list=`sed -n "/\[.*\]/p" list.conf | tr -d "[]"`
-for f in $list; do
-	echo "make $f"
-	tar cfz "$f.tar.gz" "$f"
-done;
+if test -d FTP; then
+	echo "Please remove FTP directory"
+else
+	mkdir FTP
+	cp list.conf FTP
+	for f in $list; do
+		echo "make $f"
+		rm -f "$f/*.pyc" "$f/*~"
+		mkdir "FTP/$f"
+		tar cfz "$f.tar.gz" "$f"
+		mv "$f.tar.gz" "FTP/$f"
+	done;
+fi
