13ff3b2734ee5be976379dc8e3c1fdeae53c0919
Fabrice Rey <fabounet03@gmail.com>
2011-02-25 02:02:47 +0100

    Translator: display the 'result' category first + Quote: display the site's name if icon's name is empty + updated the .conf files to match the global pattern + make_packages.sh: check version mismatch

--- make_packages.sh
+++ make_packages.sh
@@ -1,15 +1,33 @@
 #!/bin/sh
+
+# grab the list of applets to package from the list.conf.
 list=`sed -n "/\[.*\]/p" list.conf | tr -d "[]"`
+
 if test -d FTP; then
-	echo "You've to remove FTP directory"
+	echo "You have to remove FTP directory"
+	exit 1
 else
 	mkdir FTP
 	cp list.conf FTP
 	for f in $list; do
 		echo "make $f"
+		# remove unwanted files.
 		rm -f "$f/*.pyc" "$f/*~"
-		mkdir "FTP/$f"
+		
+		# check that the version in noth .conf are identical.
+		version1=`grep "^version *= *" "$f/auto-load.conf" | sed "s/.*= *//g"`
+		version2=`head -1 "$f/$f.conf" | sed "s/.*;//g"`
+		if test "$version1" != "$version2"; then
+			echo "  Warning: versions mismatch for $f ($version1/$version2)"
+		fi
+		
+		# build the tarball.
 		tar cfz "$f.tar.gz" "$f" --exclude="last-modif" --exclude="preview.png"
+		
+		# place it in its folder.
+		mkdir "FTP/$f"
 		mv "$f.tar.gz" "FTP/$f"
 	done;
 fi
+
+exit 0
