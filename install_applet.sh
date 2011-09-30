dcd10c72a547160d0618ee43046646333186c2f4
Matthieu Baerts <matttbe@gmail.com>
2011-09-30 16:43:27 +0200

    install_applet: added an exception for 'po' directory

--- install_applet.sh
+++ install_applet.sh
@@ -9,17 +9,26 @@ CAIRO_DOCK_PLUG_INS_EXTRAS_LP_BRANCH="cairo-dock-plug-ins-extras"
 CAIRO_DOCK_PLUG_INS_EXTRAS_USR="/usr/share/cairo-dock/plug-ins/Dbus/third-party"
 CAIRO_DOCK_PLUG_INS_EXTRAS_HOME="$HOME/.config/cairo-dock/third-party"
 
+	mkdir -p $CAIRO_DOCK_PLUG_INS_EXTRAS_HOME
+
 	rm -rf $DIR/$CAIRO_DOCK_PLUG_INS_EXTRAS_LP_BRANCH/tmp_last_modif
 	mkdir $DIR/$CAIRO_DOCK_PLUG_INS_EXTRAS_LP_BRANCH/tmp_last_modif
 
 	for i in `ls --hide=DOWNLOAD --hide=demos --hide=FTP --hide=tmp_last_modif $DIR/$CAIRO_DOCK_PLUG_INS_EXTRAS_LP_BRANCH`;do
 		if test -d $DIR/$CAIRO_DOCK_PLUG_INS_EXTRAS_LP_BRANCH/$i; then # seulement les dossiers
+			if [ "$i" = "po" ]; then
+				i="locale"
+			fi
 			if test -f $CAIRO_DOCK_PLUG_INS_EXTRAS_HOME/$i/last-modif ;then # backup des last-modif's
 				mkdir $DIR/$CAIRO_DOCK_PLUG_INS_EXTRAS_LP_BRANCH/tmp_last_modif/$i
 				cp $CAIRO_DOCK_PLUG_INS_EXTRAS_HOME/$i/last-modif $DIR/$CAIRO_DOCK_PLUG_INS_EXTRAS_LP_BRANCH/tmp_last_modif/$i/last-modif
 			fi
 			rm -rf $CAIRO_DOCK_PLUG_INS_EXTRAS_HOME/$i
-			ln -s $DIR/$CAIRO_DOCK_PLUG_INS_EXTRAS_LP_BRANCH/$i $CAIRO_DOCK_PLUG_INS_EXTRAS_HOME/$i # liens symboliques
+			if [ "$i" = "locale" ]; then
+				ln -s $DIR/$CAIRO_DOCK_PLUG_INS_EXTRAS_LP_BRANCH/po $CAIRO_DOCK_PLUG_INS_EXTRAS_HOME/$i # liens symboliques
+			else
+				ln -s $DIR/$CAIRO_DOCK_PLUG_INS_EXTRAS_LP_BRANCH/$i $CAIRO_DOCK_PLUG_INS_EXTRAS_HOME/$i # liens symboliques
+			fi
 			# cp ln -s $DIR/$CAIRO_DOCK_PLUG_INS_EXTRAS_LP_BRANCH/$i $CAIRO_DOCK_PLUG_INS_EXTRAS_HOME/$i
 			if test -f $DIR/$CAIRO_DOCK_PLUG_INS_EXTRAS_LP_BRANCH/tmp_last_modif/$i/last-modif ;then # backup des last-modif's
 				cp $DIR/$CAIRO_DOCK_PLUG_INS_EXTRAS_LP_BRANCH/tmp_last_modif/$i/last-modif $CAIRO_DOCK_PLUG_INS_EXTRAS_HOME/$i/last-modif
