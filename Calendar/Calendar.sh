a422b28ed51cb0640d786f3464738361503b3c93
Matthieu Baerts <matttbe@gmail.com>
2012-04-02 15:34:21 +0200

    Calendar: Used a verbadim mode (a monospace font) to write the calendar on the dialog  + Only displayed dialogs without timeout  + a few code cleaning

--- Calendar/Calendar.sh
+++ Calendar/Calendar.sh
@@ -35,27 +35,26 @@ calendar_command=`get_conf_param "calendar_command"`
 import_command=`get_conf_param "import_command"`
 
 icon_command=`get_conf_param "icon_script"`
-
-time_dialog_cal=`get_conf_param "time_dialog_cal_today"`
-
-time_dialog_ev=`get_conf_param "time_dialog_ev"`
-
-time_dialog_cal_next=`get_conf_param "time_dialog_cal_next"`
-
 }
 
 #############################################################################################################
 on_click() {
 
-time_dialog_cal_today=`get_conf_param "time_dialog_cal_today"`
 which cal > /dev/null
 if [ $? -eq 0 ]; then
-	dbus-send --session --dest=$DBUS_NAME $DBUS_PATH $DBUS_INTERFACE.applet.ShowDialog  string:"`cal -h`" int32:$time_dialog_cal_today
+	#dbus-send --session --dest=$DBUS_NAME $DBUS_PATH $DBUS_INTERFACE.applet.ShowDialog  string:"`cal -h`" int32:$time_dialog_cal_today
+	MESSAGE="<tt>`cal -h`</tt>"
+	python -c "import dbus; message=\"\"\"$MESSAGE\"\"\"; print dbus.Bus().call_blocking(
+		\"$DBUS_NAME\",
+		\"$DBUS_PATH\",
+		\"$DBUS_INTERFACE.applet\",
+		'PopupDialog',
+		'a{sv}a{sv}',
+		({'message': message, 'use-markup':True},{},))"
 else
 	echo "$APP_NAME applet -> Script Name : $SCRIPT_NAME -> 'cal' isn't installed"
 	call ShowDialog "string:\"ERROR: 'cal' isn't installed\"" "int32:5"
 fi
-#echo "$APP_NAME applet -> Script Name : $SCRIPT_NAME -> Left clic !"
 exit
 }
 
@@ -64,59 +63,25 @@ on_middle_click() {
 calendar_command=`get_conf_param "calendar_command"`
 
 $calendar_command
-
-#echo "$APP_NAME applet -> Script Name : $SCRIPT_NAME -> Middle clic !"
 exit
 }
 
 #############################################################################################################
 on_scroll_icon() {
 
-date_TD=`date +%Y%m%d%H%M%S`
-
 if [ $1 -eq 1 ]; then
 	# Scroll UP
 
-	time_dialog_cal_next=`get_conf_param "time_dialog_cal_next"`
-
-	if test -e .wait; then
-		date_WAIT=`head -n 1 .wait`
-		if [ $date_TD -ge $(($date_WAIT+4)) ];then
-			rm -f .wait_month .wait_year .wait1 # we wait for 4 sec
-		fi
-	fi
-
 	which cal > /dev/null
 	if [ $? -eq 0 ]; then
-	MONTH_NEXT=1
-	YEAR_NEXT=1
-		if test -e ".wait_month" -a -e ".wait_year"; then
-			MONTH_NEXT_READ=`head -n 1 .wait_month`
-			YEAR_NEXT_READ=`head -n 1 .wait_year`
-			MONTH_NEXT=$(($MONTH_NEXT_READ+1))
-			if [ $MONTH_NEXT -eq 13 ]; then
-				MONTH_NEXT=1
-				YEAR_NEXT=$(($YEAR_NEXT_READ+1))
-			else
-				YEAR_NEXT=$YEAR_NEXT_READ
-			fi
-			echo $MONTH_NEXT > .wait_month
-			echo $YEAR_NEXT > .wait_year
-		else
-			MONTH_NEXT=$((`date +%m`+1))
-			if [ $MONTH_NEXT -eq 13 ]; then
-				MONTH_NEXT=1
-				date_YEAR=`date +%Y`
-				YEAR_NEXT=$(($date_YEAR+1))
-			else
-				YEAR_NEXT=`date +%Y`
-			fi
-			echo $MONTH_NEXT > .wait_month
-			echo $YEAR_NEXT > .wait_year
-		fi
-		dbus-send --session --dest=$DBUS_NAME $DBUS_PATH $DBUS_INTERFACE.applet.ShowDialog  string:" Next month :
-`cal -h $MONTH_NEXT $YEAR_NEXT`" int32:$time_dialog_cal_next
-		echo $date_TD > .wait
+		MESSAGE="<tt>`cal -h -3`</tt>"
+		python -c "import dbus; message=\"\"\"$MESSAGE\"\"\"; print dbus.Bus().call_blocking(
+			\"$DBUS_NAME\",
+			\"$DBUS_PATH\",
+			\"$DBUS_INTERFACE.applet\",
+			'PopupDialog',
+			'a{sv}a{sv}',
+			({'message': message, 'use-markup':True},{},))"
 	else
 		echo "$APP_NAME applet -> Script Name : $SCRIPT_NAME -> 'cal' isn't installed"
 		call ShowDialog "string:\"ERROR: 'cal' isn't installed\"" "int32:5"
@@ -125,8 +90,6 @@ if [ $1 -eq 1 ]; then
 else
 	# Scroll DOWN
 
-	time_dialog_ev=`get_conf_param "time_dialog_ev"`
-
 	which calendar > /dev/null
 	if [ $? -nq 0 ]; then
 		echo "$APP_NAME applet -> Script Name : $SCRIPT_NAME -> 'calendar' isn't installed"
@@ -134,21 +97,9 @@ else
 		exit
 	fi
 
-	if test -e .wait1; then
-		date_WAIT=`head -n 1 .wait1`
-		if [ $date_TD -ge $(($date_WAIT+3)) ];then
-			# we wait for 3 sec
-			calendar -f /usr/share/calendar/calendar.all > /dev/null # The loading takes some time :-/
-			sleep 0.5
-			dbus-send --session --dest=$DBUS_NAME $DBUS_PATH $DBUS_INTERFACE.applet.ShowDialog  string:"`calendar -f /usr/share/calendar/calendar.all`" int32:$time_dialog_ev
-		else
-			exit
-		fi
-	else
-		dbus-send --session --dest=$DBUS_NAME $DBUS_PATH $DBUS_INTERFACE.applet.ShowDialog  string:"`calendar`" int32:$time_dialog_ev
-	fi
-
-	echo $date_TD > .wait1
+	calendar -f /usr/share/calendar/calendar.all > /dev/null # The loading takes a few time :-/
+	sleep 0.5
+	dbus-send --session --dest=$DBUS_NAME $DBUS_PATH $DBUS_INTERFACE.applet.ShowDialog  string:"`calendar -f /usr/share/calendar/calendar.all`" int32:0
 fi
 
 exit
@@ -171,8 +122,8 @@ begin() {
 cp $CONF_FILE $CONF_FILE.bak
 # Generate fresh calendar icon
 get_ALL_conf_params
-rm -f .wait .wait1 .wait_month .wait_year .day
-if test `ps aux | grep -c "update_calendar"` -gt 1; then
+rm -f .day
+if test `ps aux | grep -c "[u]pdate_calendar"` -gt 1; then
 	killall update_calendar.sh
 fi
 (bash update_calendar.sh "$icon_command" &)
@@ -182,7 +133,7 @@ exit
 
 #############################################################################################################
 end() {
-rm -f .wait .wait1 .wait_month .wait_year .day
+rm -f .day
 if test `ps aux | grep -c "update_calendar"` -gt 1; then
 	killall update_calendar.sh
 fi
@@ -192,13 +143,7 @@ fi
 reload() {
 diff $CONF_FILE $CONF_FILE.bak >/dev/null
 if [ $? -eq 1 ]; then
-	cp $CONF_FILE $CONF_FILE.bak
-	get_ALL_conf_params
-	rm -f .wait .wait1 .wait_month .wait_year .day
-	if test `ps aux | grep -c "update_calendar"` -gt 1; then
-		killall update_calendar.sh
-	fi
-	(bash update_calendar.sh "$icon_command" &)
+	begin
 fi
 exit
 }
