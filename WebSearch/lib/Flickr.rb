1ad39f0e64b312a493ab3318ca159d2dba1e7b85
Eduardo Mucelli R. Oliveira <edumucelli@gmail.com>
2013-06-15 21:59:02 -0300

    Fixed Flickr

--- WebSearch/lib/Flickr.rb
+++ WebSearch/lib/Flickr.rb
@@ -16,11 +16,12 @@ class Flickr < Engine
 	# url, e.g., /photos/21078069@N03/2780732654/
 	# thumb_url, e.g., http://farm4.static.flickr.com/3255/2780732654_b7cbb2fb98_t.jpg"
 	def retrieve_links(query, page = 1)
-		flickr = Nokogiri::HTML(open(URI.encode("#{self.query_url}#{query}#page=#{page}")))
-		(flickr/"span[@class='photo_container pc_t']/a").each do |res|
-			url = res['href']
-			description = res['title']
-			thumb_url = res.at("img")['src']
+		flickr = Nokogiri::HTML(open(URI.encode("#{self.query_url}#{query}#page=#{page}"), "User-Agent" => self.user_agent))
+		(flickr/"div[@class^='thumb']").each do |res|
+		  photo = res.at("a[@class='rapidnofollow photo-click']")
+	    url = photo['href']
+	    description = photo['title']
+	    thumb_url = photo.at("img")['data-defer-src']
 			self.links << ThumbnailedLink.new("#{self.base_url}#{url}", description, thumb_url, self.name)
 		end
 		self.links
