03666a9a92016b658cb331756870f4470a731cab
Matthieu Baerts <matttbe@gmail.com>
2014-11-03 22:41:57 +0100

    dependences_deb: remove ruby deps
    no longer used with stable and up to date applets

--- dependences_deb.sh
+++ dependences_deb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NEEDED_EXTRAS="python libxslt1-dev libxml2-dev xclip valac ruby rubygems"
+NEEDED_EXTRAS="python libxslt1-dev libxml2-dev xclip valac"
 
 LOG_CD=../log.txt
 
@@ -29,19 +29,5 @@ install_pkg() {
 }
 
 install_pkg 2> /dev/null
-
-# written in vala, we have to use valac.
-if test -d 'CairoBzr'; then
-	cd CairoBzr
-	./compile.sh >> $LOG_CD 2>> ../$LOG_CD
-	if [ $? -ne 0 ]; then ERROR=1; fi
-	cd ..
-fi
-
-if test ! -f '../.ruby'; then
-	touch ../.ruby
-	sudo gem install parseconfig addressable launchy nokogiri ruby-dbus
-	if [ $? -ne 0 ]; then ERROR=1; fi
-fi
 
 if test -n "$ERROR"; then exit 1; fi
